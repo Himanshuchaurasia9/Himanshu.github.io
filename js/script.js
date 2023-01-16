4db953d8eaf75225f9477b36e449ed1322afbbbe
Himanshu Chaurasia <106426822+Himanshuchaurasia9@users.noreply.github.com>
2023-01-16 10:33:23 +0530

    This is my own personal portfolio website.
    This is my own personal portfolio website.
    I am very glad to make this website.
    Himanshu Personal Portfolio.

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const menu= document.querySelector("#menu-icon");
+const navlist= document.querySelector(".navlist");
+
+menu.addEventListener("click",()=>{
+    menu.classList.toggle("open");
+    navlist.classList.toggle("open");
+})
+document.querySelectorAll(".navlist").forEach(n => n.addEventListener("click", ()=>{
+    menu.classList.remove("open");
+    navlist.classList.remove("open");
+}))
+// ACTIVE LINK
+const section =document.querySelectorAll("section");
+const links = document.querySelectorAll("a");
+
+window.onscroll = ()=>{
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop-150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if(top >= offset && top < offset + height){
+            links.forEach(link =>{
+                link.classList.remove("active");
+                document.querySelector("header ul li a[href*="+ id +"]").classList.add("active");
+            })
+        }
+    })
+}
+
+
+// TYPED SCRIPT HOME
+
+var typed =  new Typed(".auto-type",{
+    strings: ["Web Developer","Web Designer","Frontend Developer","Python Developer","Wordpress"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
+// TYPED SCRIPT FOOTER
+
+var typed =  new Typed(".hero",{
+    strings: ["Himanshu Chaurasia &#x2764;"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
+
