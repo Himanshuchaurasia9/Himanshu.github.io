6637a2907acc9c7648627d052d86b3c6f80ec114
Himanshu Chaurasia <106426822+Himanshuchaurasia9@users.noreply.github.com>
2023-08-03 12:56:31 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,3 +47,17 @@ var typed =  new Typed(".hero",{
     loop: true
 })
 
+
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
+
